ff3cdcf5cd52b45efe32a71fcdd504158bd8a860
ZGss1234 <z22x22c22@naver.com>
2022-08-20 02:13:51 +0900

    적 총알 삭제, 플레이어 무적시간 추가
    ㅁㄴㅇㄹ

--- Assets/BulletEnemy.cs
+++ Assets/BulletEnemy.cs
@@ -9,6 +9,9 @@ public class BulletEnemy : MonoBehaviour
 
     public float dmg;
 
+    public float zmin;
+    public float zmax;
+
     public void SetData(float _spd, float _dir, float _dmg)
     {
         spd = _spd;
@@ -20,5 +23,13 @@ public class BulletEnemy : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3.right * Mathf.Cos(dir) + Vector3.forward * Mathf.Sin(dir)) * spd * Time.deltaTime;
+
+        if (transform.position.z < zmin || transform.position.z > zmax) Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var tag = collision.gameObject.tag;
+        if (tag == "side") Destroy(gameObject);
     }
 }
--- Assets/ShipHp.cs
+++ Assets/ShipHp.cs
@@ -7,16 +7,41 @@ public class ShipHp : MonoBehaviour
     public float hp_max = 10f;
     public float hp;
 
+    public float invi_time_max;
+    float invi_time;
+    bool invi;
+
     // Start is called before the first frame update
     void Start()
     {
         hp = hp_max;
+        invi = false;
+        invi_time = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (invi_time > 0)
+        {
+            invi_time -= Time.deltaTime;
+            if(invi_time <= 0)
+            {
+                invi_time = 0f;
+                invi = false;
+            }
+        }
     }
 
     void GetDmg(float amount)
     {
-        hp -= amount;
-        if (hp <= 0) Debug.Log("Game Over");
+        if (invi == false)
+        {
+            hp -= amount;
+            if (hp <= 0) Debug.Log("Game Over");
+
+            invi = true;
+            invi_time = invi_time_max;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
